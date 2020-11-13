189867a3a0f6793f047c1f621f6ef7d679eb7872
Raul Hernandez Fernandez <raulhernandezfernandez@gmail.com>
2020-11-13 14:53:39 +0000

    Update arrays.c: Introduce the allocation and reallocation

--- C-tutorials/07_arrays/arrays.c
+++ C-tutorials/07_arrays/arrays.c
@@ -8,9 +8,11 @@
 int main(){
 
 	int a_size = 0;
+	int new_a_size = 0;
+	int variable_array[a_size];
+
 	printf("Insert the array length\n");
 	scanf(" %d",&a_size);
-	int variable_array[a_size];
 
 	for(int indx=0; indx < a_size; indx++){
 		variable_array[indx] = indx;
@@ -18,23 +20,39 @@ int main(){
 	}
 
 	/*Allocating memory to an array*/
-	printf("Allocating memory region.\n");
+	printf("Allocating memory\n");
 
 	int *pm = NULL;
-
-        if(pm == NULL) {
-	    fprintf(stderr, "Error allocating memory!\n");
-            exit(1);
-} 
-
 	pm = malloc(a_size * sizeof(int));
-
+        if( pm == NULL ) {
+	    fprintf(stderr, "Error allocating memory: line 27!\n");
+            exit(1);
+	}
 	for( int indx = 0; indx < a_size; indx++){
 		pm[indx] = indx;
 		printf("For indx = %d result = %d\n", indx, *(pm + indx));
 	}
 
-	free(pm);
+	/*Reallocating memory*/
+	printf("Reallocating memory.\n");
+
+	printf("Introduce the array new size\n");
+	scanf("%d",&new_a_size);
+
+	int *tmp = NULL;
+	tmp = realloc(pm, new_a_size*sizeof(int));
+	if (tmp == NULL){
+		fprintf(stderr, "Error re-allocating memory: line 39\n");
+		exit(1);
+	}
+
+	printf("Memory realocated\n");	
+	for(int indx = 0; indx < new_a_size; indx++){
+	tmp[indx] = indx;
+	printf("a[%d] = %d.\n", indx, tmp[indx]);
+	}
+
+free(tmp);
 
 return 0;
 }
