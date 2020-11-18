ef1d776d4ee0f8c473fca16bcf614a969e2dc4a6
Raul Hernandez Fernandez <raulhernandezfernandez@gmail.com>
2020-11-18 10:01:04 +0000

    Add function_pointers.c

--- C-tutorials/11_files/files.c
+++ C-tutorials/11_files/files.c
@@ -1,15 +1,39 @@
 #include<stdio.h>
 #include<stdlib.h>
 
+typedef struct{
+	int r;
+	int b;
+	int g;
+}Pixel;
+
 int main(){
 
 char *my_file = "my_file.txt";
+Pixel p[50];
+Pixel p2[50];
 
 FILE *pf = fopen(my_file, "w");
-
 fprintf(pf, "Introduce:\n");
-
 fclose(pf);
 
+for(int indx = 0; indx < 50; indx ++){
+	p[indx].r=2;
+	p[indx].b=3;
+	p[indx].g=4;
+}
+pf = fopen("test.bin", "wb");
+fwrite(p,sizeof(Pixel), 50, pf);
+fclose(pf);
+
+pf = fopen("test.bin", "rb");
+fread(p2, sizeof(Pixel), 50, pf);
+for(int indx = 0; indx < 50; indx ++){
+printf("%d %d %d\n",
+	p2[indx].r,
+	p2[indx].b,
+	p2[indx].g);
+}
+fclose(pf);
 return 0;
 }
---
+++ C-tutorials/12_function_pointers/function_pointers.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+
+typedef double (*pfunc)(double, double);
+
+double add(double, double);
+double subs(double, double);
+double mult(double, double);
+
+void operation(pfunc, double, double);
+
+int main(){
+
+	pfunc calc;
+	double a = 10;
+	double b = 11;
+
+	operation(add,  a, b);
+	operation(subs, a, b);
+	operation(mult, a, b);
+
+	return 0;
+}
+
+void operation(pfunc fp, double a, double b){
+
+	double result = fp(a,b);
+
+	printf("result = %0.2lf\n", result);
+} 
+
+double add(double a, double b){
+
+	return a + b;
+}
+double subs(double a, double b){
+
+	return a - b;
+}
+double mult(double a, double b){
+
+	return a * b;
+}
